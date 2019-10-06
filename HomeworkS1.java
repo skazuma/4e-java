93c6da8a98296de8098407d79a000c8cb82f47a5
Kazuma Sone <skazuma518@gmail.com>
2019-10-06 14:47:27 +0900

    後期プログラミングⅢ課題1

---
+++ HomeworkS1.java
@@ -0,0 +1,28 @@
+import tg.*;
+
+public class HomeworkS1 {
+  public static void main(String[] args){
+    int n = 5;
+    int k = 3;
+    int size = 100;
+    if(args.length==0){
+      TurtleFrame f;
+      f =  new TurtleFrame();
+      Turtle m = new Turtle();
+      f.add(m);
+      for(int i = 0; i < n; i++){
+          m.fd(size); m.rt(k * 360.0 / n);
+      }
+    }
+    else{
+      TurtleFrame f;
+      f =  new TurtleFrame();
+      HTurtle m = new HTurtle();
+      f.add(m);
+      for(int i = 0; i < n; i++){
+          m.fd(size); m.rt(k * 360.0 / n);
+          m.polygon(6,50);
+      }
+    }
+  }
+}
