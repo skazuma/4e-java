865f7a0ae910d4abfdc647eab8d01dab29f0a723
Kazuma Sone <skazuma518@gmail.com>
2019-11-15 21:07:43 +0900

    課題6

--- EvaluateSetRelation.java
+++ EvaluateSetRelation.java
@@ -1,45 +1,50 @@
 import java.io.*;
 import java.util.*;
 public class EvaluateSetRelation{
-  void evaluate(String[] inFile){
-    Set<String> listH = new HashSet<String>();
+  void evaluate(String strA, String strB){
+    Set<String> setA = new HashSet<String>();
+    Set<String> setB = new HashSet<String>();
+    String inFile[];
+    inFile = new String[2];
+    inFile[0] = strA;
+    inFile[1] = strB;
 
-    //Scanner[] sc = new Scanner[inFile.length];
-    String separator = "(\\s+?|\\.|,)";
+    String separator = "(\\s+?|\\.|\\(|\\)|,|;)";
+
     for(int i=0; i<inFile.length; i++){
       try(BufferedReader br = new BufferedReader(new FileReader(inFile[i]));){
         String str = br.readLine();
-        listH.clear();
         while(str != null){
           String[] words = str.split(separator);
           for(String word : words){
             if(!word.isEmpty()){
-              listH.add(word);
+              if(i==0){
+                setA.add(word);
+              }
+              else{
+                setB.add(word);
+              }
             }
           }
-          /*
-          sc[i] = new Scanner(str);
-          sc[i].useDelimiter(" ");
-          while(sc[i].hasNext()){
-            listH.add(sc[i].next());
-          }
-          */
+
           str = br.readLine();
         }
-        //sc[i].close();
       }catch(IOException error){System.out.println("ÉGÉâÅ[î≠ê∂");}
-      System.out.println(listH);
     }
+    System.out.println(setA);
+    System.out.println(setB);
+    Set<String> intersection = new HashSet<String>(setA);
+    intersection.retainAll(setB);
+    Set<String> union = new HashSet<String>(setA);
+    union.addAll(setB);
+    double jaccard = (double)intersection.size()/union.size();
+    System.out.println("JaccardåWêî:"+jaccard);
   }
 
   public static void main(String[] args){
-    String inFile[];
-    inFile = new String[2];
-    inFile[0] = "Batman1.txt";
-    inFile[1] = "Joker1.txt";
     EvaluateSetRelation esr = new EvaluateSetRelation();
 
-    esr.evaluate(inFile);
+    esr.evaluate("Batman.txt", "Joker.txt");
 
   }
 }
